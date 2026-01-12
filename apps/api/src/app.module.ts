2079bb17e0c9c05ab935f901cc628d9ba28abc2a
Yoongu <yoonguk510@gmail.com>
2026-01-12 16:52:03 +0900

    feat(api): setup orpc module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
-
+import { REQUEST } from '@nestjs/core';
+import { onError, ORPCModule } from '@orpc/nest';
+import { Request } from 'express';
+import { SmartCoercionPlugin } from '@orpc/json-schema'
 import { DatabaseModule } from './database/database.module';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    ORPCModule.forRootAsync({
+      useFactory: (request: Request) => ({
+        context: { request },
+        plugins: [
+            new SmartCoercionPlugin(),
+        ],
+        interceptors: [
+          onError((error) => {
+            console.error('ORPC Error:', error);
+          }),
+        ],
+      }),
+      inject: [REQUEST],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
